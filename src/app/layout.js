062c6e91706d3bdfd12c718b5d99a841e520b763
Bayzid004 <bayzid4440@gmail.com>
2025-06-26 17:18:13 +0600

    setup

---
+++ src/app/layout.js
@@ -0,0 +1,24 @@
+import { Montserrat } from 'next/font/google'
+import './globals.css'
+
+const montserrat = Montserrat({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+  display: 'swap'
+})
+
+export const metadata = {
+  title: 'Grand Landscaping',
+  description: 'Grand Landscaping'
+}
+
+export default function RootLayout ({ children }) {
+  return (
+    <html lang='en'>
+      <body className={`${montserrat.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  )
+}
