76372b23777d2ea73e0ae16eb828c92fb73bc675
Bayzid004 <bayzid4440@gmail.com>
2025-06-27 23:08:00 +0600

    removed

--- src/components/homePage/companies/Companies.jsx
+++
@@ -1,8 +0,0 @@
-export default function Companies() {
-  return (
-    <div className="container text-center space-y-4 py-10">
-        <h2 className="text-pink-500 font-bold text-4xl">COMPANIES</h2>
-        <h1 className="text-5xl font-bold">Trusted by great brands</h1>
-    </div>
-  );
-}
